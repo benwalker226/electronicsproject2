9e79ad7a1ba532055f3bb69413733ea6cbe5c4ab
bqr511 <bqr511@york.ac.uk>
2026-01-06 16:32:29 +0000

    Update lcr_measure.h
    main code for the collection of results from lcr_measure.c
    
    defines the values, voltage, current, and the feedback resistor state (highRange) 
    
    and then defines the function that will measure the values of the components

--- lcr_measure.h
+++ lcr_measure.h
@@ -1,1 +1,15 @@
+#ifndef LCR_MEASURE_H
+#define LCR_MEASURE_H
 
+#include <stdint.h>
+#include <STDbool.h>
+
+typedef struct {
+  float voltage;
+  float current;
+  float highRange; // Feedback resistor state
+} LCR_Measurement_t;
+
+void LCR_DoMeasurement(LCR_Measurement_t *result);
+
+#endif
