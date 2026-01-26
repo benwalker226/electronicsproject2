1bf8f4533e1bd4560eb33b1067a34b6415b974d0
VivekMuvva <vivekmuvva@gmail.com>
2026-01-26 16:35:04 +0000

    Create green_section.h

---
+++ green_section.h
@@ -0,0 +1,15 @@
+/*
+ * ui.h
+ *
+ *  Created on: 05 Jan 2026
+ *      Author: Vivek
+ */
+
+#ifndef UI_H
+#define UI_H
+
+#include "lcr_measure.h"
+
+void UI_GreenSectionLoop(const LCR_Measurement_t *m);
+
+#endif
