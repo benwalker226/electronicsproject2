f2d97dfb5afb84835e30c178f24a41c3bc9a79bc
bqr511 <bqr511@york.ac.uk>
2026-01-28 19:31:28 +0000

    Update DAJP_F303K8_Driver.c

--- DAJP_F303K8_Driver.c
+++ DAJP_F303K8_Driver.c
@@ -330,13 +330,16 @@ void LCR_ADC_GetSamples(uint32_t *data, uint32_t length, uint32_t rate) {
 	// two samples are returned for each trigger, so the data array needs to
 	// have a size of at least length * 2.
 
+	DMA1->IFCR = DMA_IFCR_CGIF1; 
     // Set up DMA1 channel one for the ADC capture (ADC is always on DMA Channel one):
     DMA1_Channel1->CCR &= ~0x01;    // Disable the DMA channel
     DMA1_Channel1->CPAR = (uint32_t) &ADC1->DR;  // Set peripheral address (source of data)
     DMA1_Channel1->CMAR = (uint32_t) data;       // Set memory address (destination of data)
     DMA1_Channel1->CNDTR = length * 2;           // Number of transfers
-    DMA1_Channel1->CCR = 0x1A80;    // Set priority, increments, circular mode, bit-widths, etc
-    DMA1_Channel1->CCR |= 0x01;     // Enable the DMA channel
+    // DMA1_Channel1->CCR = 0x1A80;    // Set priority, increments, circular mode, bit-widths, etc
+    // DMA1_Channel1->CCR |= 0x01;     // Enable the DMA channel
+	DMA1_Channel1->CCR = 0;
+	DMA1_Channel1->CCR |= DMA_CCR_PL_1; 
 
     // Set-up TIM2 to time the ADC conversions:
     uint32_t DivRatio = SystemCoreClock / rate;
@@ -355,9 +358,10 @@ void LCR_ADC_GetSamples(uint32_t *data, uint32_t length, uint32_t rate) {
     TIM2->CR1 |= TIM_CR1_CEN;    // Set the timer going
 
     // Now wait for the DMA to complete before returning:
-    while (DMA1_Channel1->CNDTR > 0) {
-    	LCR_MicroDelay(1000);
-    }
+    while ((DMA1->ISR & DMA_ISR_TCIF1) == 0) {
+		LCR_MicroDelay(1000); // timeout protection
+	}
+	DMA1->IFCR = DMA_IFCR_CTCIF1;
     TIM2->CR1 &= ~TIM_CR1_CEN;    // Stop the timer
 }
 
@@ -472,6 +476,7 @@ void LCR_FuncGen_Update(uint32_t rate) {
     // And re-enable TIM6:
     TIM6->CR1 |= TIM_CR1_CEN;
 }
+
 
 
 
